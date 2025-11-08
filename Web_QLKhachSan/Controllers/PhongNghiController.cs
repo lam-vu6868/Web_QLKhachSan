4d5d0a7f60f26216daa926f4be171f3dac528954
Le Tuan Hai <23050116@student.bdu.edu.vn>
2025-11-08 10:09:04 +0700

    Thêm Sổ dữ liệu Loại Dòng và tiện ích

--- Web_QLKhachSan/Controllers/PhongNghiController.cs
+++ Web_QLKhachSan/Controllers/PhongNghiController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_QLKhachSan.Models;
 
 namespace Web_QLKhachSan.Controllers
 {
     public class PhongNghiController : Controller
     {
+        private DB_QLKhachSanEntities db = new DB_QLKhachSanEntities();
+
         // GET: PhongNghi
         public ActionResult Index()
         {
-            return View();
+            // Lấy danh sách loại phòng từ database
+            var loaiPhongs = db.LoaiPhongs.Where(lp => lp.NgayTao != null).ToList();
+            ViewBag.LoaiPhongs = loaiPhongs;
+
+            // Lấy danh sách tiện ích từ database
+            var tienIches = db.TienIches.Where(ti => ti.NgayTao != null).ToList();
+            ViewBag.TienIches = tienIches;
+
+            // Lấy danh sách phòng với thông tin liên quan
+            var phongs = db.Phongs.Include("LoaiPhong")
+                                  .Include("PhongAnhs")
+                                  .Where(p => p.DaHoatDong == true)
+                                  .ToList();
+            
+            return View(phongs);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
